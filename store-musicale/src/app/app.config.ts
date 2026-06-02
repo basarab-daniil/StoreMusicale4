d5e6213b86c418a6559cec0df5426b52de726f77
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-06-02 13:07:15 +0000

    fix: zone.js

--- store-musicale/src/app/app.config.ts
+++ store-musicale/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    // Rimuove la dipendenza da Zone.js e abilita la reattività Zoneless nativa
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient()
   ]
